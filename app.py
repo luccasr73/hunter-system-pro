0df4cc1b8d048ed0b1023bf8aef0e4560394abf9
luccasr73 <luccasrobert@hotmail.com>
2020-04-27 01:39:03 -0300

    Caminho do pasta de arquivos estaticos

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 import router
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='/static')
 router.setRoutes(app)
- 
+
 if __name__ == '__main__':
     app.run(debug=True)
