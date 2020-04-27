856f962c40a7507ec49c273458b34b07416b32c3
luccasr73 <luccasrobert@hotmail.com>
2020-04-27 01:38:37 -0300

    Renderização de template baseada no tipo de usuario

--- routes/cadastro.py
+++ routes/cadastro.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, request, render_template
-
+from flask import Blueprint, request, render_template, redirect, url_for
 cadastro = Blueprint('cadastro', __name__, url_prefix="/",
                   template_folder='../view')
 
 @cadastro.route("/cadastro")
 def index():
-    return "cadastro"
+    tipo = request.args.get('tipo')
+    print(tipo)
+    if tipo == 'candidato':
+        return render_template('cadastro.html')
+    if tipo == 'admin':
+        return 'admin'
+    if tipo != 'admin' or tipo != 'candidato':
+        return redirect(url_for('home.index'))
