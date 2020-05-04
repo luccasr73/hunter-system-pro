3aa7a597484268afcfca31f5254157cbe86241a4
luccasr73 <luccasrobert@hotmail.com>
2020-05-04 01:33:55 -0300

    Alteração nas rotas

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(session({
     httpOnly: true
   }
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -31,13 +33,12 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: false
 }))
-app.use(passport.initialize())
-app.use(passport.session())
+
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', rotas.index)
-app.use('/cadastro', rotas.cadastro)
+app.use('/', rotas.comum)
+app.use('/candidato', rotas.candidato)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
