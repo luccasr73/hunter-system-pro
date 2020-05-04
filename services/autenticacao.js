6fbc15acca0c855f375b8f5e021d19790c1f08c5
luccasr73 <luccasrobert@hotmail.com>
2020-05-04 01:35:19 -0300

    Correção para o login funcionar

--- services/autenticacao.js
+++ services/autenticacao.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-const login = require('../models/login')
+const Login = require('../models/login')
 const bcrypt = require('bcryptjs')
 
 function compararSenha (senha, hash) {
@@ -12,7 +12,10 @@ passport.serializeUser((id, done) => {
 })
 
 passport.deserializeUser((id, done) => {
-  login.buscarPorId(id)
+  const login = new Login({
+    id: id
+  })
+  login.buscarPorId()
     .then((user) => {
       done(null, user)
     })
@@ -21,21 +24,41 @@ passport.deserializeUser((id, done) => {
     })
 })
 
-const strategy = new LocalStrategy(async (usuario, senha, done) => {
+passport.use('login', new LocalStrategy({
+  usernameField: 'usuario',
+  passwordField: 'senha'
+}, async (usuario, senha, done) => {
+  console.log(usuario)
+  console.log(senha)
   try {
-    usuario = login.buscar(usuario)
+    const login = new Login({
+      usuario: usuario
+    })
+    usuario = await login.buscarPorUsuario()
     if (!usuario) {
-      return done(null, false)
+      console.log('usuario n existe')
+      return done(null, false, { message: 'Usuario incorreto' })
     }
     if (!await compararSenha(senha, usuario.senha)) {
-      return done(null, false)
+      console.log('senha errada')
+      return done(null, false, { message: 'Senha incorreta' })
     } else {
       return done(null, usuario)
     }
   } catch (error) {
     return done(error)
   }
-})
-passport.use(strategy)
+}))
 
-module.exports = passport
+const authenticationMiddleware = () => {
+  return function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/index')
+  }
+}
+module.exports = {
+  authenticationMiddleware,
+  passport
+}
