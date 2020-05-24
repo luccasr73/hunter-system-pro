aca051e2136d74a448b6376c66d08b481b99ec7a
luccasr73 <luccasrobert@hotmail.com>
2020-05-24 04:51:35 -0300

    Update comum.js

--- routes/comum.js
+++ routes/comum.js
@@ -3,17 +3,19 @@ const router = express.Router()
 const autenticacao = require('../services/autenticacao')
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'teste' })
+  res.render('index', {
+    tituloPagina: 'Inicio'
+  })
 })
 
 router.get('/login', function (req, res, next) {
-  console.log(req.isAuthenticated())
   res.render('login')
 })
 
 router.post('/login', autenticacao.passport.authenticate('login', {
   successRedirect: '/candidato/perfil',
   failureRedirect: '/login'
+  // failureFlash: true
 }))
 
 module.exports = router
