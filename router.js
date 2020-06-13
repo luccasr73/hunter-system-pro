766939034521ef4b01d94752b86df4ec4716d468
luccasr73 <luccasrobert@hotmail.com>
2020-06-13 03:31:51 -0300

    Update router.js

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 const comum = require('./routes/comum')
 // const usersRouter = require('./routes/users')
 const candidato = require('./routes/candidato')
+const admin = require('./routes/admin')
 module.exports = {
   comum,
+  admin,
   candidato
 }
