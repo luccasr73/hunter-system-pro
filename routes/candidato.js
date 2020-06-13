808f4782b89bfa8ada6177dc01674447781042af
luccasr73 <luccasrobert@hotmail.com>
2020-06-13 02:17:23 -0300

    Crud idioma/curso

--- routes/candidato.js
+++ routes/candidato.js
@@ -6,6 +6,8 @@ const Candidato = require('../models/candidato')
 const Endereco = require('../models/endereco')
 const EnderecoCandidato = require('../models/enderecoCandidato')
 const Experiencia = require('../models/experiencia')
+const Curso = require('../models/curso')
+const Idioma = require('../models/idioma')
 const Template = require('../utils/Template')
 const moment = require('moment')
 const {
@@ -30,6 +32,9 @@ router.get('/curriculo', middleware.usuarioEstaLogado(), async function (req, re
   const enderecoCandidato = await EnderecoCandidato.buscar(candidato.id)
   const endereco = await Endereco.buscar(enderecoCandidato.id_endereco)
   const experiencias = await Experiencia.buscarPorCandidato(candidato.id)
+  const cursos = await Curso.buscarPorCandidato(candidato.id)
+  const idiomas = await Idioma.buscarPorCandidato(candidato.id)
+  console.log(cursos)
   res.render('curriculo', {
     tituloPagina: 'Curriculo',
     candidato,
@@ -37,6 +42,8 @@ router.get('/curriculo', middleware.usuarioEstaLogado(), async function (req, re
     experiencias,
     usuario,
     moment,
+    cursos,
+    idiomas,
     TemplateUtils: Template
   })
 })
@@ -176,6 +183,80 @@ router.delete('/curriculo/experiencia', middleware.usuarioEstaLogado(), async fu
   const data = req.body
   try {
     await Experiencia.deletar(data.id)
+    res.json(data)
+  } catch (error) {
+    res.json(error)
+    throw new Error(error)
+  }
+})
+
+router.get('/curriculo/cursos', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  const usuario = req.user
+  try {
+    const candidato = await Candidato.buscarPorIdLogin(usuario.id)
+    const cursos = await Experiencia.buscarPorCandidato(candidato.id)
+    res.json(cursos)
+  } catch (error) {
+    res.status(500).send('erro')
+    throw new Error(error)
+  }
+})
+
+router.post('/curriculo/curso', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  const usuario = req.user
+  const data = req.body
+  try {
+    const candidato = await Candidato.buscarPorIdLogin(usuario.id)
+    const id = await Curso.criar(candidato.id, data.nome, data.instituicao, data.data_formacao)
+    data.id = id[0]
+    res.json(data)
+  } catch (error) {
+    res.json(error)
+    throw new Error(error)
+  }
+})
+router.delete('/curriculo/curso', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  // const usuario = req.user
+  const data = req.body
+  try {
+    await Curso.deletar(data.id)
+    res.json(data)
+  } catch (error) {
+    res.json(error)
+    throw new Error(error)
+  }
+})
+
+router.get('/curriculo/idiomas', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  const usuario = req.user
+  try {
+    const candidato = await Candidato.buscarPorIdLogin(usuario.id)
+    const idiomas = await Idioma.buscarPorCandidato(candidato.id)
+    res.json(idiomas)
+  } catch (error) {
+    res.status(500).send('erro')
+    throw new Error(error)
+  }
+})
+
+router.post('/curriculo/idioma', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  const usuario = req.user
+  const data = req.body
+  try {
+    const candidato = await Candidato.buscarPorIdLogin(usuario.id)
+    const id = await Idioma.criar(candidato.id, data.descricao, data.nivel)
+    data.id = id[0]
+    res.json(data)
+  } catch (error) {
+    res.json(error)
+    throw new Error(error)
+  }
+})
+router.delete('/curriculo/idioma', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  // const usuario = req.user
+  const data = req.body
+  try {
+    await Idioma.deletar(data.id)
     res.json(data)
   } catch (error) {
     res.json(error)
