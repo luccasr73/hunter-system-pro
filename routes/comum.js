1f44b3df1a6fd58626d1bae0d13fae424036d517
luccasr73 <luccasrobert@hotmail.com>
2020-06-14 03:04:34 -0300

    Tratamento para quando o usuario já está inscrito na vaga, rota para inscrever na vaga

--- routes/comum.js
+++ routes/comum.js
@@ -4,6 +4,8 @@ const { passport, middleware } = require('../services/autenticacao')
 const Vaga = require('../models/vaga')
 const Empresa = require('../models/empresa')
 const Template = require('../utils/Template')
+const CandidatoVaga = require('../models/candidatoVaga')
+const Candidato = require('../models/candidato')
 router.get('/', function (req, res, next) {
   const usuario = req.user
   res.render('index', {
@@ -45,29 +47,29 @@ router.get('/vaga/:id', middleware.usuarioEstaLogado(), async function (req, res
   const idVaga = req.params.id
   const vaga = await Vaga.buscar(idVaga)
   const empresa = await Empresa.buscar(vaga.id_empresa)
+  const candidato = await Candidato.buscarPorIdLogin(usuario.id)
+  const estaInscrito = Vaga.estaInscrito(vaga.id, candidato.id)
   console.log(vaga)
   res.render('vaga', {
     tituloPagina: 'Vagas',
     usuario,
     empresa,
-    vaga
+    vaga,
+    estaInscrito
   })
 })
 
-router.get('/vaga/:id/candidatar', middleware.usuarioEstaLogado(), async function (req, res, next) {
+router.post('/vaga/candidatar', middleware.usuarioEstaLogado(), async function (req, res, next) {
   const usuario = req.user
-  // console.log(req.body)
-  // console.log(req.flash('error'))
-  const idVaga = req.params.id
-  const vaga = await Vaga.buscar(idVaga)
-  const empresa = await Empresa.buscar(vaga.id_empresa)
-  console.log(vaga)
-  res.render('vaga', {
-    tituloPagina: 'Vagas',
-    usuario,
-    empresa,
-    vaga
-  })
+  const data = req.body
+  try {
+    const candidato = await Candidato.buscarPorIdLogin(usuario.id)
+    console.log(candidato)
+    await CandidatoVaga.criar(data.id_vaga, candidato.id)
+    res.status(200).send(data)
+  } catch (error) {
+    res.send(error)
+  }
 })
 
 router.get('/sair', function (req, res, next) {
