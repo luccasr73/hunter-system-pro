fc0b364df99b11e30842970084756570e34fc0bb
luccasr73 <luccasrobert@hotmail.com>
2020-06-14 00:31:57 -0300

    Rotas para vagas

--- routes/comum.js
+++ routes/comum.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const { passport, middleware } = require('../services/autenticacao')
-
+const Vaga = require('../models/vaga')
+const Empresa = require('../models/empresa')
 router.get('/', function (req, res, next) {
   const usuario = req.user
   res.render('index', {
@@ -14,10 +15,56 @@ router.get('/entrar', middleware.usuarioNaoEstaLogado(), function (req, res, nex
   const usuario = req.user
   // console.log(req.body)
   // console.log(req.flash('error'))
+
   res.render('entrar', {
     tituloPagina: 'Entrar',
     erroLogin: req.flash('error'),
     usuario
+  })
+})
+
+router.get('/vagas', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  const usuario = req.user
+  // console.log(req.body)
+  // console.log(req.flash('error'))
+  const vagas = await Vaga.buscarTodasComEmpresa()
+  console.log(vagas)
+  res.render('vagas', {
+    tituloPagina: 'Vagas',
+    usuario,
+    vagas
+  })
+})
+
+router.get('/vaga/:id', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  const usuario = req.user
+  // console.log(req.body)
+  // console.log(req.flash('error'))
+  const idVaga = req.params.id
+  const vaga = await Vaga.buscar(idVaga)
+  const empresa = await Empresa.buscar(vaga.id_empresa)
+  console.log(vaga)
+  res.render('vaga', {
+    tituloPagina: 'Vagas',
+    usuario,
+    empresa,
+    vaga
+  })
+})
+
+router.get('/vaga/:id/candidatar', middleware.usuarioEstaLogado(), async function (req, res, next) {
+  const usuario = req.user
+  // console.log(req.body)
+  // console.log(req.flash('error'))
+  const idVaga = req.params.id
+  const vaga = await Vaga.buscar(idVaga)
+  const empresa = await Empresa.buscar(vaga.id_empresa)
+  console.log(vaga)
+  res.render('vaga', {
+    tituloPagina: 'Vagas',
+    usuario,
+    empresa,
+    vaga
   })
 })
 
