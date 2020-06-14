074aabc09f9005666d604faeb97e032b3ae2e99a
luccasr73 <luccasrobert@hotmail.com>
2020-06-14 00:28:57 -0300

    Metodos para desativar/buscar todas e correções

--- models/vaga.js
+++ models/vaga.js
@@ -1,13 +1,13 @@
 const knex = require('../db/knex')
 
 class Vaga {
-  static criar (idEmpresa, cargo, tags, ativo, resposabilidades, requisitos, transacao = null) {
+  static criar (idEmpresa, cargo, tags, ativo, responsabilidades, requisitos, transacao = null) {
     const data = {
       id_empresa: idEmpresa,
       cargo,
       ativo,
       tags,
-      resposabilidades,
+      responsabilidades,
       requisitos
     }
     if (transacao) {
@@ -18,11 +18,11 @@ class Vaga {
       .insert(data).returning('*')
   }
 
-  static atualizar (idVaga, cargo, tags, resposabilidades, requisitos, transacao = null) {
+  static atualizar (idVaga, cargo, tags, responsabilidades, requisitos, transacao = null) {
     const data = {
       cargo,
       tags,
-      resposabilidades,
+      responsabilidades,
       requisitos
     }
     if (transacao) {
@@ -50,6 +50,19 @@ class Vaga {
       }).del()
   }
 
+  static desativar (idVaga, transacao = null) {
+    if (transacao) {
+      return transacao('vaga')
+        .where({
+          id: idVaga
+        }).update({ ativo: 0 })
+    }
+    return knex('vaga')
+      .where({
+        id: idVaga
+      }).update({ ativo: 0 })
+  }
+
   static buscar (idVaga, transacao = null) {
     if (transacao) {
       return transacao('vaga')
@@ -62,6 +75,49 @@ class Vaga {
         id: idVaga
       }).first()
   }
+
+  static buscarTodas (transacao = null) {
+    if (transacao) {
+      return transacao('vaga')
+        .where({
+          ativo: 1
+        })
+    }
+    return knex('vaga')
+      .where({
+        ativo: 1
+      })
+  }
+
+  static buscarTodasComEmpresa (transacao = null) {
+    if (transacao) {
+      return transacao('vaga')
+        .join('empresa as EMP', 'EMP.id', '=', 'vaga.id')
+    }
+    return knex('vaga')
+      .join('empresa as EMP', 'EMP.id', '=', 'vaga.id')
+  }
+
+  // if (transacao) {
+  //   return transacao('vaga')
+  //     .join('endereco_vaga', 'vaga.id', '=', 'endereco_vaga.id_vaga')
+  //     .join('endereco', 'endereco.id', '=', 'endereco_vaga.id_endereco')
+  //     .join('empresa', 'empresa.id', '=', 'vaga.id')
+  //     .join('endereco_empresa', 'endereco_empresa.id_empresa', '=', 'empresa.id')
+  //     .join('endereco', 'endereco.id', '=', 'endereco_empresa.id_endereco')
+  //     .where({
+  //       ativo: 1
+  //     }).toSQL().toNative()
+  // }
+  // return knex('vaga')
+  //   .join('endereco_vaga', 'vaga.id', '=', 'endereco_vaga.id_vaga')
+  //   .join('endereco', 'endereco.id', '=', 'endereco_vaga.id_endereco')
+  //   .join('empresa', 'empresa.id', '=', 'vaga.id')
+  //   .join('endereco_empresa', 'endereco_empresa.id_empresa', '=', 'empresa.id')
+  //   .join('endereco', 'endereco.id', '=', 'endereco_empresa.id_endereco')
+  //   .where({
+  //     ativo: 1
+  //   }).toSQL().toNative()
 }
 
 module.exports = Vaga
